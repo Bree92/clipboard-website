80aa84947a07a6ac570c09a1b846a0f999a95fda
YangBoo92 <bcbcoding@gmail.com>
2023-02-27 17:08:02 +0800

    grid testimonial

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,10 @@ module.exports = {
       xl: "1440px",
     },
     extend: {
-      colors: {
-        strongCyan: "hsl(171, 66%, 44%)",
-        lightBlue: "hsl(233, 100%, 69%)",
-        darkGrayishBlue: "hsl(210, 10%, 33%)",
-        grayishBlue: "hsl(201, 11%, 66%)",
-      },
       fontFamily: {
-        sans: ["Bai Jamjuree", "sans-serif"],
-      },
-      backgroundImage: {
-        "hero-pattern-desktop": "url('/images/bg-header-desktop.png')",
-        "hero-pattern-mobile": "url('/images/bg-header-mobile.png')",
+        sans: ["Barlow Semi Condensed", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
